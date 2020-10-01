0bab4e9897873e2320452210df65cbea255edeb0
Steven Topazio <sat@northpointprinting.com>
2020-10-01 14:19:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,13 +29,7 @@ class Song
   end
   
   def self.find_by_name(title)
-    result == self.find_by_name(title)
-    if result
-      result
-    else 
-      self.create_by_name(title)
-    end
-  end
+    result == self.all.detect 
   
   def 
     
